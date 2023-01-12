e5159eddbdfd21c2a4cd46bc670765a55e625280
Md Imran Hossen <freelancerimran81@gmail.com>
2023-01-12 16:14:12 +0600

    category section done

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,11 @@
-import { View, Text, Image, TextInput } from 'react-native'
+import { View, Text, Image, TextInput, ScrollView } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Entypo from 'react-native-vector-icons/Entypo';
+import Categories from '../components/Categories';
 
 export default function HomeScreen() {
   const navigation = useNavigation()
@@ -18,52 +19,65 @@ export default function HomeScreen() {
   return (
     <SafeAreaView className="bg-white pt-3">
 
-        {/* Header Section */}
-        <View>
-          <View className="flex-row pb-3 items-center mx-4 space-x-2">
-            <Image
-              source={{
-                uri: "https://links.papareact.com/wru"
-              }}
-              className="h-7 w-7 bg-gray-300 p-4 rounded-full"
-            />
-            <View className="flex-1">
-              <Text className="font-bold text-gray-400 text-xs"> Deliver Now!</Text>
-              <Text className="font-bold text-xl"> Current Location
-                <Ionicons
-                  name="chevron-down"
-                  size={20}
-                  color="#00CCBB"
-                />
-              </Text>
-            </View>
-            <View>
-              <FontAwesome
-                name="user-o"
+      {/* Header Section */}
+      <View>
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+          <Image
+            source={{
+              uri: "https://links.papareact.com/wru"
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+          <View className="flex-1">
+            <Text className="font-bold text-gray-400 text-xs"> Deliver Now!</Text>
+            <Text className="font-bold text-xl"> Current Location
+              <Ionicons
+                name="chevron-down"
                 size={20}
                 color="#00CCBB"
               />
-            </View>
+            </Text>
           </View>
-        </View>
-
-        {/* Search Bar */}
-
-        <View>
           <View>
             <FontAwesome
-              name="search"
+              name="user-o"
               size={20}
               color="#00CCBB"
             />
-            <TextInput className="border w-full" />
           </View>
-          <Entypo
-            name="sound-mix"
+        </View>
+      </View>
+
+      {/* Search Bar */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row space-x-2 flex-1 bg-gray-200 p-3 items-center rounded">
+          <FontAwesome
+            name="search"
             size={20}
-            color="#00CCBB"
+            color="gray"
+          />
+          <TextInput
+            placeholder='Restaurants and cuisines'
+            keyboardType='default'
           />
         </View>
+        <Entypo
+          name="sound-mix"
+          size={20}
+          color="#00CCBB"
+        />
+      </View>
+
+      {/* Body */}
+      <ScrollView className="bg-gray-100 "
+
+      >
+        {/* category's */}
+        <Categories/>
+
+        {/* Featured Rows */}
+      </ScrollView>
+
     </SafeAreaView>
   )
 }
